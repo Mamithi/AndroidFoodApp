8ff0553f52b3e93f618993fd5cb8d84c616f7f2a
Mamithi <19lawrence93@gmail.com>
2018-01-31 00:59:54 +0300

    Create home page with sign up and sign in buttons

--- app/src/main/java/com/mamithi/delivery/MainActivity.java
+++ app/src/main/java/com/mamithi/delivery/MainActivity.java
@@ -1,13 +1,43 @@
 package com.mamithi.delivery;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "";
+    Button btnSignUp, btnSignIn;
+    TextView txtSlogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        txtSlogan = findViewById(R.id.txtSlogan);
+        Typeface face = Typeface.createFromAsset(getAssets(), "fonts/Nabila.ttf");
+        txtSlogan.setTypeface(face);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
