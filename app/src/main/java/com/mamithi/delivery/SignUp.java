cad703b7237cedb35a72ba92f5f95cd4ddb548d9
Mamithi <19lawrence93@gmail.com>
2018-01-31 02:29:33 +0300

    Add logic for registering a new user

--- app/src/main/java/com/mamithi/delivery/SignUp.java
+++ app/src/main/java/com/mamithi/delivery/SignUp.java
@@ -1,12 +1,20 @@
 package com.mamithi.delivery;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.mamithi.delivery.Model.User;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class SignUp extends AppCompatActivity {
@@ -26,10 +34,38 @@ public class SignUp extends AppCompatActivity {
 
         btnSignUp = findViewById(R.id.btnSignUp);
 
+        // Init Firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_user = database.getReference("user");
+
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final ProgressDialog mDialog = new ProgressDialog(SignUp.this);
+                mDialog.setMessage("Please wait...");
+                mDialog.show();
 
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // Check if the user already exists in the database
+                        if(dataSnapshot.child(editPhone.getText().toString()).exists()){
+                            mDialog.dismiss();
+                            Toast.makeText(SignUp.this, "This Phone Number is already registered", Toast.LENGTH_SHORT).show();
+                        } else{
+                            mDialog.dismiss();
+                            User user = new User(editName.getText().toString(), editPassword.getText().toString());
+                            table_user.child(editPhone.getText().toString()).setValue(user);
+                            Toast.makeText(SignUp.this, "Registration successful", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
             }
         });
     }
